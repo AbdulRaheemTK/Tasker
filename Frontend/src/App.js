91644a9ecc02ebb9ffdccace4194fe09825f1c47
AbdulRaheemTK <abdulraheemtahirkhan@gmail.com>
2023-02-02 14:14:23 +0500

    Added navigation restrictions

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,8 +3,22 @@ import Login from "./features/Login/Login";
 import Signup from "./features/Signup/Signup";
 import { Routes, Route } from "react-router-dom";
 import Home from "./features/Home/Home";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const { user } = useSelector((state) => state.login);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  }, [user]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
